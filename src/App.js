fc26592558786a7cbf27c9cdd2a7ef3b7a55afac
Matt <matias_moll@yahoo.com.ar>
2020-07-21 21:00:35 -0300

    Added basic component structure. Added image resources for the thematic of the app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Topbar from './components/layout/Topbar'
+import Quizzes from './components/quizzes/Quizzes'
+import EndGame from './components/quizzes/EndGame'
+import Scoreboard from './components/quizzes/Scoreboard'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Free Code Quiz</h1>
-    </div>
+    <Router>
+      <div className="App">
+          <Topbar></Topbar>
+          <h1>Free Code Quiz</h1>
+          <Switch>
+            <Route exact path="/" component={Quizzes}></Route>
+            <Route exact path="/endgame" component={EndGame}></Route>
+            <Route exact path="/scoreboard" component={Scoreboard}></Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/quizzes/Quizzes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Quizzes extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
