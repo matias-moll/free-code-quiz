456cadd174c3d4ecee7e8e1f8049df75fae007a7
Matt <matias_moll@yahoo.com.ar>
2020-07-28 23:57:59 -0300

    Added about text

--- src/components/community/About.js
+++ src/components/community/About.js
@@ -4,6 +4,10 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
+
+      <p>Free Code Quiz is an Open Source, Free project created by Matt Moll to help developers practice and flex their web development muscles while having fun and competing for the top spots of the leaderboard.</p> 
+      <p> I have always been passionate about learning, teaching and competitions so this was a great opportunity for me to put everything together and help the community.</p>
+      <p>If you would like to help you can collaborate adding questions to the app and we will review them and approve if they are not repeated and are correct. You can also collaborate to the codebase here: (github link)</p>    
     </div>
   )
 }
