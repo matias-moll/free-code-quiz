4ae91a3d416031b101bebe9c8e4d4449f41f6f54
Matt <matias_moll@yahoo.com.ar>
2020-08-11 22:50:23 -0300

    Updated Lywo quotes styles. Still unsure about the default message after every tip on new question.

--- src/components/quizzes/Lywo.js
+++ src/components/quizzes/Lywo.js
@@ -21,9 +21,13 @@ export default class Lywo extends Component {
     const { hint } = this.props;
     return (
       <div id="lywo-container">
-        <p id="lywo-message"> {this.state.showHint? hint : 
-        "I´m Lywo and I will be your guide in these quizzes! Hope you have fun. You can tap on me for hints."}</p>
-        <a href="!#" onClick={this.onLywoClicked}><img src={lywoImg} alt="Lywo Character" style={{width:120, height:120}}/></a>
+        <div>
+          <p id="lywo-message"> {this.state.showHint? hint : 
+            `I´m Lywo and I will be your guide! I hope you have fun while learning.
+             You can tap on me for hints.`}
+          </p>
+        </div>
+        <a href="!#" onClick={this.onLywoClicked}><img src={lywoImg} alt="Lywo Character" style={{width:150, height:150}}/></a>
       </div>
     )
   }
